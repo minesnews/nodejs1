2d4f01090f58769ade80f82b3620283d7ce5a36f
Nikolay Chelpanov <nikch3008@mail.ru>
2025-03-01 17:20:18 +0700

    upd hw

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const http = require('http');
-var countMain = 1;
-var countAbout = 1;
-var count404 = 1;
-var count = 1;
+var countMain = 0;
+var countAbout = 0;
+var count404 = 0;
+var count = 0;
 const server = http.createServer((req, res) => {
+    count++;
     console.log("Запрос получен");
     if (req.url === '/') {
+        countMain++;
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=utf-8'
         });
         res.end('<a href="/about">Перейти на страницу обо мне</a><p><h1>Просмотров страницы main:' + countMain + '</h1><p><h1>Просмотров всех страниц сайта:' + count + '</h1>');
-        countMain++;
     } else if (req.url === '/about') {
+        countAbout++;
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=utf-8'
         });
         res.end('<a href="/">Перейти на страницу главная</a><p><h1>Просмотров страницы about:' + countAbout + '</h1><p><h1>Просмотров всех страниц сайта:' + count + '</h1>');
-        countAbout++;
     } else {
+        count404++;
         res.writeHead(404, {
             'Content-Type': 'text/html; charset=utf-8'
         });
         res.end('<h1>Страница 404</h1><p><h1>Просмотров страницы 404:' + count404 + '</h1><p><h1>Просмотров всех страниц сайта:' + count + '</h1>');
-        count404++;
     }
-    count++;
 });
 
 const port = '3000';
